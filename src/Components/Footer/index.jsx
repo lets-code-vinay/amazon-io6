33554d04c28f5d5548812c032b17fd6eb14e3d9b
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2024-08-01 19:17:08 +0530

    Making footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+const Footer=()=>{
+    return(
+        <>
+        <Box className="main-footer-container">
+
+<h1>hello</h1>
+        </Box>
+
+        </>
+    )
+};
+
+
+export default Footer;
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header"
 import SubHeader from "../../components/Sub-header"
+import Footer from "../../components/Footer";
 import "./style.css"
 
 const Homepage=()=>{
@@ -8,6 +9,8 @@ return(
     <>
     <Header/>
     <SubHeader/>
+    <h1>hello</h1>
+    <Footer/>
     </>
 );
 };
